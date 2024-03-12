d4be22b62d3b2133aa6d5e84060b7362af63354b
Jenn <znzhenn@gmail.com>
2024-03-12 09:24:15 -0400

    introduces the gameboard
    introduces the gameboard

--- TicTacToe.java
+++ TicTacToe.java
@@ -14,6 +14,7 @@ public class TicTacToe {
                                 {'-', '+', '-','+','-'},
                                 {' ', '|', ' ','|',' '}};
 
+        System.out.println("This is the game board: ");
         printGameBoard(gameBoard);
 
         while (true) {
